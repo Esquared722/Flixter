6980ac4fa90449cd7ae1182fc390ce85c112b841
Eric <esinnicke@gmail.com>
2022-02-07 18:44:50 -0500

    Unit 2: Added release date year and genres to detail page.

--- app/src/main/java/com/example/flixter/models/Movie.java
+++ app/src/main/java/com/example/flixter/models/Movie.java
@@ -1,12 +1,19 @@
 package com.example.flixter.models;
 
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.parceler.Parcel;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import okhttp3.Headers;
 
 @Parcel
 public class Movie {
@@ -16,7 +23,15 @@ public class Movie {
     String backdropPath;
     String title;
     String overview;
+    String releaseDate;
+    String[] genres;
     double rating;
+
+    private static Map<Integer, String> genreMap;
+    private static JSONArray genresJSONArray;
+    private static int num_genres;
+
+    private static final String GENRE_MAP = "https://api.themoviedb.org/3/genre/movie/list?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US";
 
     // empty constructor needed by Parcel library
     public Movie(){}
@@ -28,6 +43,40 @@ public class Movie {
         overview = jsonObject.getString("overview");
         rating = jsonObject.getDouble("vote_average");
         movieId = jsonObject.getInt("id");
+        releaseDate = jsonObject.getString("release_date");
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(GENRE_MAP, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                try {
+                    genresJSONArray = json.jsonObject.getJSONArray("genres");
+                    num_genres = genresJSONArray.length();
+                    genreMap = getGenreMap();
+                    JSONArray genreCodeArray = jsonObject.getJSONArray("genre_ids");
+                    int numGenres = genreCodeArray.length();
+                    genres = new String[numGenres];
+                    for(int i = 0; i < numGenres; i++) {
+                        genres[i] = genreMap.get(genreCodeArray.getInt(i));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+
+            }
+        });
+    }
+
+    private static Map<Integer, String> getGenreMap() throws JSONException {
+        Map<Integer, String> genreMap = new HashMap<>();
+        for(int i = 0; i < num_genres; i++) {
+            JSONObject genre = genresJSONArray.getJSONObject(i);
+            genreMap.put(genre.getInt("id"), genre.getString("name"));
+        }
+        return genreMap;
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -60,5 +109,21 @@ public class Movie {
 
     public int getMovieId() {
         return movieId;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate.split("-")[0];
+    }
+
+    public String getGenres() {
+        StringBuilder genreString = new StringBuilder();
+        for (int i = 0; i < genres.length; i++) {
+            if (i == (genres.length - 1)) {
+                genreString.append(genres[i]);
+                break;
+            }
+            genreString.append(genres[i]).append(" | ");
+        }
+        return genreString.toString();
     }
 }
